0538efae96a385fc3e94409e3b37f58fa4325c0f
marcus aurelio <marcusaurelio743@gmail.com>
2024-07-01 11:14:38 -0300

    resolvendo exercicio usando array

---
+++ Arrays/src/ex1Application/Program.java
@@ -0,0 +1,37 @@
+package ex1Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import ex1Entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		Product[] vect = new Product[n];
+		
+		for(int i = 0; i < vect.length; i++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			vect[i] = new Product(name, price);
+		}
+		
+		double sum = 0.00;
+		for(int i=0; i < vect.length; i++) {
+			sum+= vect[i].getPrice();
+		}
+		
+		double media = sum /vect.length;
+		
+		System.out.printf("media dos preços; %.2f",media);
+		sc.close();
+
+	}
+
+}
